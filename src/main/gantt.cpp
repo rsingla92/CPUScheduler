31e869cb4f091a412a1983d9df731161c1dd172e
Rohit Singla <rsingla92@gmail.com>
2013-03-07 09:59:33 -0800

    Added Gantt class with constructor, getQueue, and display and metrics functions.

--- src/main/gantt.cpp
+++ src/main/gantt.cpp
@@ -4,4 +4,23 @@
  * Authors : Lauren Fung, Jorden Hetherington
  *           Jeremy Lord, Rohit Singla
  */
+#include "gantt.h"
 
+Gantt::Gantt( std::vector<ProcessControlBlock> queue) : _queue(queue) {}
+
+void Gantt::display()
+{
+    std::vector<ProcessControlBlock>::iterator it = getQueue().begin();
+
+    std::string chart = "| "
+    for( ; it != getQueue.end(); ++it) {
+	chart += itoa(it->getPID()) + " |"; 
+    }
+
+   std::cout << chart << endl;
+}
+
+std::vector<ProcessControlBlock> Gantt::getQueue()
+{
+   return _queue;
+}
--- src/main/gantt.hpp
+++ src/main/gantt.hpp
@@ -4,3 +4,15 @@
  * Authors : Lauren Fung, Jorden Hetherington
  *           Jeremy Lord, Rohit Singla
  */
+
+#include "ProcessControlBlock.h"
+
+class Gantt
+{
+   public:
+       Gantt( std::vector<ProcessControlBlock> queue );
+       void display();
+       void metrics();
+   private:
+     std::vector<ProcessControlBlock> queue;
+}
