e56921bfdbdf9124089b36c1a26e3df9a7e1be06
laurenf720 <laurenf720@gmail.com>
2013-03-15 19:58:29 -0700

    preemptive SJF implementation
    resolves issue #42.
    thanks to jeremy/ricky's magic functions.
    everything is easy.
    im kind of sad its so easy. hahaha

--- src/main/PreemptiveShortestJobFirst.cpp
+++ src/main/PreemptiveShortestJobFirst.cpp
@@ -7,12 +7,32 @@
 
 #include "PreemptiveShortestJobFirst.hpp"
 
+using std::cout;
+using std::endl;
+using std::vector;
+
 PreemptiveShortestJobFirst:: PreemptiveShortestJobFirst(std::vector<ProcessControlBlock> rawData, int quantumTime) : Algorithm(rawData, quantumTime){
-    std::cout << "make a PSJF object" << std::endl;
+    cout << "make a PSJF object" << endl;
 }
 
 void PreemptiveShortestJobFirst:: run(){
-    std::cout << "running PSJF with preemption" << std::endl;
+    cout << "running PSJF with preemption" << endl;
+    
+    populateInitialQueues(isShorterCPUBurst);
+    
+    while (true){
+        checkWaitingProcesses();
+        if(_readyQueue.size() == 0) break;
+        
+        if(_readyQueue[0].getCPUBursts().size() != 0){
+            vector<ProcessControlBlock>::size_type oldReadyQSize = _readyQueue.size();
+            preempt(isShorterCPUBurst);
+            
+            if(_readyQueue.size() >= oldReadyQSize){
+                std::sort(_readyQueue.begin(), _readyQueue.end(), isShorterCPUBurst);
+            }
+        }
+    }
 }
 
 std::vector<ProcessControlBlock> PreemptiveShortestJobFirst::getFinalQueueOrder()
