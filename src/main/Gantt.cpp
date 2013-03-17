a7dc023dca2994530e5b334a6d2ca7b833296236
Rohit Singla <rsingla92@gmail.com>
2013-03-17 12:50:19 -0700

    Added a timesMap to Gantt. Stores needed information and makes for ease of use. Included overall throughput and turnAround time for each process.

--- src/main/Gantt.cpp
+++ src/main/Gantt.cpp
@@ -7,7 +7,14 @@
 #include "Gantt.hpp"
 
 typedef std::vector<AlgorithmData>::iterator itr;
-typedef std::map<int,int> waitMap;
+
+typedef struct {
+    int ioTime;
+    int waitTime;
+    int turnAroundTime;
+} times;
+
+typedef std::map<int,times> timeMap;
 
 Gantt::Gantt( std::vector<AlgorithmData> queue) : _queue(queue) {}
 
@@ -41,12 +48,33 @@ void Gantt::display()
 
 void Gantt::metrics()
 {
-   waitMap waitTimes;
+   timeMap pidTimes;
+   int totalBurstTime = 0;
+   int totalProcesses = 0;
 
    for(itr it =_queue.begin(); it != _queue.end(); ++it) {
-       waitTimes[it->PID] = it->waitTime;
+       pidTimes[it->PID].waitTime = it->waitTime;
+       pidTimes[it->PID].ioTime = it->IOTime;
+       pidTimes[it->PID].turnAroundTime = (it->burstTime + it->IOTime + it->waitTime);
+       totalBurstTime += it->burstTime;
    }
 
+   // if there was an IDLE placeholder, ignore it.
+   totalProcesses = (pidTimes.find(-1)  != pidTimes.end()) ? pidTimes.size()-1 : pidTimes.size();
+
+   typedef std::map<int,times>::iterator map_itr;
+
+   for(map_itr index = pidTimes.begin(); index != pidTimes.end(); ++index) {
+       if(index->first == -1) {
+           continue;
+       } else {
+           std::cout << "PID " << index->first << " turnAroundTime time " << index->second.turnAroundTime << std::endl;
+       } 
+   }
+
+   std::cout << "The total CPU execution time was " << totalBurstTime << std::endl;
+   std::cout << "The number of processes executed was " << totalProcesses << std::endl;
+   std::cout << "The throughput of this run per one time unit was " << (float) totalProcesses/totalBurstTime << std::endl;
    return;
 }
 
