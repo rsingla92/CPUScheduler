ad6cc1aebc8e8c1b3eb9f360558a8d6f7a96924c
Rohit Singla <rsingla92@gmail.com>
2013-03-16 22:07:51 -0700

    Started work on the display function. Iterates through and prints out PID or IDLE. Started work on metrics. Adjussted types to be of type AlgorithmData.

--- src/main/Gantt.cpp
+++ src/main/Gantt.cpp
@@ -6,22 +6,51 @@
  */
 #include "Gantt.hpp"
 
-Gantt::Gantt( std::vector<ProcessControlBlock> queue) : _queue(queue) {}
+typedef std::vector<AlgorithmData>::iterator itr;
+typedef std::map<int,int> waitMap;
+
+Gantt::Gantt( std::vector<AlgorithmData> queue) : _queue(queue) {}
 
 void Gantt::display()
 {
-    std::vector<ProcessControlBlock> temp(this->getQueue());
-    std::vector<ProcessControlBlock>::iterator it = temp.begin();
 
-    std::string chart = "| ";
-    for( ; it != temp.end(); ++it) {
-		chart += intToString(it->getPID()) + " |"; 
-    }
+   std::string chart = "|";
+   itr it = _queue.begin();
+   
+   for( it = _queue.begin(); it != _queue.end(); ++it) {
+       int tempTime = 0;
+       while(it->PID == (it+1)->PID) {
+            tempTime += it->burstTime;
+            it++;
+       }
+
+       // kk now what.
+   }
+
+
+   for(it = _queue.begin() ; it != _queue.end(); ++it) {
+       if( it->PID == IDLE) {
+           chart += "IDLE | ";
+       } else {
+           chart += intToString(it->PID) + " | ";
+       }
+   }
 
    std::cout << chart << std::endl;
 }
 
-std::vector<ProcessControlBlock> Gantt::getQueue()
+void Gantt::metrics()
+{
+   waitMap waitTimes;
+
+   for(itr it =_queue.begin(); it != _queue.end(); ++it) {
+       waitTimes[it->PID] = it->waitTime;
+   }
+
+   return;
+}
+
+std::vector<AlgorithmData> Gantt::getQueue()
 {
    return _queue;
 }
